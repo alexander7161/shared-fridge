394a612a68186234e0d9084a1b8081826566b241
FreyaNoir <freyanoir@gmail.com>
2018-11-24 20:14:23 +0200

    login screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -7,6 +7,7 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
+
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,30 +1,278 @@
 import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Alert
+} from "react-native";
 
-class SignInScreen extends React.Component {
+import { WebBrowser } from "expo";
+
+import { MonoText } from "../components/StyledText";
+
+export default class LoginScreen extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
   static navigationOptions = {
-    title: "Please sign in"
+    header: null
+  };
+
+  onClickListener = viewId => {
+    Alert.alert("Alert", "Button pressed " + viewId);
   };
 
   render() {
+    var d = new Date();
+    var time = d.getHours();
+
     return (
       <View style={styles.container}>
-        <Button title="Sign in!" onPress={this._signInAsync} />
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require("../assets/images/logo.png")
+                  : require("../assets/images/logo.png")
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            {this._maybeRenderDevelopmentModeWarning()}
+
+            <Text style={styles.getStartedText}>
+              {time > 18
+                ? "Good evening!"
+                : time > 12
+                ? "Good afternoon!"
+                : "Good morning!"}
+            </Text>
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputIcon}
+              source={{
+                uri: "https://png.icons8.com/message/ultraviolet/50/3498db"
+              }}
+            />
+            <TextInput
+              style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid="transparent"
+              onChangeText={email => this.setState({ email })}
+            />
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Image
+              style={styles.inputIcon}
+              source={{
+                uri: "https://png.icons8.com/key-2/ultraviolet/50/3498db"
+              }}
+            />
+            <TextInput
+              style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid="transparent"
+              onChangeText={password => this.setState({ password })}
+            />
+          </View>
+          <TouchableHighlight
+            style={[styles.buttonContainer, styles.loginButton]}
+            onPress={this._signInAsync}
+          >
+            <Text style={styles.loginText}>Login</Text>
+          </TouchableHighlight>
+        </ScrollView>
       </View>
     );
   }
 
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      );
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use
+          useful development tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync(
+      "https://docs.expo.io/versions/latest/guides/development-mode"
+    );
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      "https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes"
+    );
+  };
   _signInAsync = async () => {
     // await AsyncStorage.setItem("userToken", "abc");
     this.props.navigation.navigate("Main");
   };
 }
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff"
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: "rgba(0,0,0,0.4)",
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: "center"
+  },
+  contentContainer: {
+    paddingTop: 30
+  },
+  welcomeContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10
+  },
+  getStartedContainer: {
+    alignItems: "center",
+    marginHorizontal: 50
+  },
+  homeScreenFilename: {
+    marginVertical: 7
+  },
+  codeHighlightText: {
+    color: "rgba(96,100,109, 0.8)"
+  },
+  codeHighlightContainer: {
+    backgroundColor: "rgba(0,0,0,0.05)",
+    borderRadius: 3,
+    paddingHorizontal: 4
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center"
+  },
+  tabBarInfoContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3
+      },
+      android: {
+        elevation: 20
+      }
+    }),
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
+  },
+  navigationFilename: {
+    marginTop: 5
+  },
+  inputs: {
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20,
+    justifyContent: "center",
+    textAlign: "center"
+  },
+  inputContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: "#FFFFFF",
+    flex: 1
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 45,
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30
+  },
+  loginButton: {
+    backgroundColor: "#ff471a",
+    flex: 1,
     alignItems: "center",
     justifyContent: "center"
+  },
+  loginText: {
+    color: "white"
   }
 });
-
-export default SignInScreen;
